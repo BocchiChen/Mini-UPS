75acde7f55badf33cafac0d58779efcf05f1c9fe
Xiuyuan Chen <74497884+BocchiChen@users.noreply.github.com>
2023-04-28 11:37:26 -0400

    Update main.py

--- Docker_Compose/UPS_backend/main.py
+++ Docker_Compose/UPS_backend/main.py
@@ -1,4 +1,3 @@
-import os
 import socket
 import select
 import threading
@@ -10,40 +9,45 @@ import interface
 #truck num
 TRUCK_NUM = 10
 
+def connectToFrontend():
+  print("Starting listening frontend request ...")
+  thread = threading.Thread(target = interface.acceptFConnection)
+  thread.start()
+
 if __name__ == "__main__":
   try:
     #connect to world and amazon
-    print("Starting UPS...")
+    print("Starting UPS Backend ...")
     world_socket = world.connectToWorldServer()
-    pid = os.fork()
-    if pid == 0:
-      print("Entering child process work area")
-      interface.acceptFConnection()
-    else:
-      print("Entering parent process work area")
-      amazon_socket = amazon.acceptAConnection()
+    connectToFrontend()
+
+    print("Starting interacting with world and amazon ...")
+    #amazon_socket = amazon.acceptAConnection() #block
   
-      while amazon.getWorldID() is None:
-        amazon.synchronizeWithAmazon()
+    #while amazon.getWorldID() is None:
+    #amazon.synchronizeWithAmazon()
     
-      connect = False
-      while connect is False:
-        connect = world.sayHelloToWorld(TRUCK_NUM)
-      '''
-      #select
-      fdset = [world_socket, amazon_socket]
+    connect = False
+    while connect is False:
+      connect = world.sayHelloToWorld(TRUCK_NUM)
+
+    '''
+    #select
+    fdset = [world_socket, amazon_socket]
   
-      while True:
-        rl, wl, error = select.select(fdset, [], [])
-        if len(rl) != 0:
-          for fd in rl:
-            if fd is world_socket:
-              world.worldRespRouter()
-            else:
-              amazon.amazonRespRouter()
+    while True:
+      rl, wl, error = select.select(fdset, [], [])
+      if len(rl) != 0:
+        for fd in rl:
+          if fd is world_socket:
+            print("process world socket")
+            world.worldRespRouter()
+          else:
+            print("process amazon socket")
+            amazon.amazonRespRouter()
             
-      world_socket.close()
-      amazon_socket.close()
+    world_socket.close()
+    amazon_socket.close()
     '''
   except Exception as e:
     print(e)
